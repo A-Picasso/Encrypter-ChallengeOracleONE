dae6b563ffe951533771c6dcbad2f3e66c22d8eb
Picasso-Linux <japm996@gmail.com>
2022-12-24 02:28:30 -0600

    All scripts (javaScript) implemented correctly.

---
+++ js/script.js
@@ -0,0 +1,119 @@
+/* Buttons */
+let buttonEncrypt = document.querySelector('#buttonEncrypt');
+let buttonDecrypt = document.querySelector('#buttonDecrypt');
+let buttonCopy = document.querySelector('#buttonCopy');
+
+/* Input Data */
+let inputArea = document.querySelector('#input-area');
+
+/* Output Data*/
+let outputArea = document.querySelector('#output');
+
+/* Hidden items */
+let warningText = document.querySelector('.warning-text'); /*1*/
+let errorMessage = document.querySelector('.container-error'); /*2*/
+let dataOutput = document.querySelector('.data-output'); /*3*/
+let pDefault = document.querySelector('.pDefault'); /*none*/
+let infoCopied = document.querySelector('.copied');
+
+
+/* Show functions */
+function showDataOutput(){
+  dataOutput.classList.remove('none');
+  pDefault.classList.add('none');
+}
+
+function showErrorMessage(){
+  pDefault.classList.add('none');
+  errorMessage.classList.remove('none');
+}
+
+function shoWarningText(){
+  pDefault.classList.remove('none');
+  warningText.classList.remove('none');
+  setTimeout (()=>{
+    warningText.classList.add('none');
+  }, 3000);
+}
+
+/* Principal functions */
+function clearDataOutput(){
+  errorMessage.classList.add('none');
+  dataOutput.classList.add('none');
+}
+
+function validateInput(string) {
+  const regex = /^([a-zA-Z]+[\s]*)+$/;
+  return regex.test(string);
+}
+
+function encrypt(string) {
+  let encryptedText = "";
+  encryptedText = string
+  .replaceAll('a', '1')
+  .replaceAll('e', '2')
+  .replaceAll('i', '3')
+  .replaceAll('o', '4')
+  .replaceAll('u', '5');
+  encryptedText = encryptedText
+  .replaceAll('1', 'ai')
+  .replaceAll('2', 'enter')
+  .replaceAll('3', 'imes')
+  .replaceAll('4', 'ober')
+  .replaceAll('5', 'ufat');
+  outputArea.value = encryptedText;
+  showDataOutput();
+}
+
+function decrypt(string) {
+  let decryptedText = "";
+  decryptedText = string
+  .replaceAll(/ai/g, 'a')
+  .replaceAll(/enter/g, 'e')
+  .replaceAll(/imes/g, 'i')
+  .replaceAll(/ober/g, 'o')
+  .replaceAll(/ufat/g, 'u');
+  outputArea.value = decryptedText;
+  showDataOutput();
+}
+
+
+/* Buttons Events */ 
+buttonEncrypt.addEventListener('click', ()=>{
+  clearDataOutput();
+  const inputText = inputArea.value;
+  if ( validateInput(inputText) && inputText.length >= 1 ) {
+    encrypt(inputText.toLowerCase());
+  } else if ( inputText.length <= 0 ) {
+    showErrorMessage();
+  }
+  else if ( !validateInput(inputText) ) {
+    shoWarningText();
+  }
+});
+
+buttonDecrypt.addEventListener('click', ()=>{
+  clearDataOutput();
+  const outputText = inputArea.value;
+  if ( validateInput(outputText) && outputText.length >= 1 ) {
+    decrypt(outputText.toLowerCase());
+  } else if ( outputText.length <= 0 ) {
+    showErrorMessage();
+  }
+  else if ( !validateInput(outputText) ) {
+    shoWarningText();
+  }
+});
+
+buttonCopy.addEventListener('click', ()=>{
+  navigator.clipboard.writeText(outputArea.value)
+  .then( ()=>{
+    infoCopied.classList.remove('none');
+    setTimeout(()=>{
+      infoCopied.classList.add('none');
+    }, 2000);
+  }) 
+  .catch( ()=>{
+    console.log('Syntax ERROR');
+  });
+});
