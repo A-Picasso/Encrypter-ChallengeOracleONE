7e8a773cc213d334abd492fd0a8d97d1e407409c
Picasso-Linux <japm996@gmail.com>
2023-01-16 18:55:42 -0600

    Added new background and fixed the textarea

--- js/script.js
+++ js/script.js
@@ -15,24 +15,37 @@ let outputArea = document.querySelector('.output');
 let body = document.body;
 
 /* Hidden items */
-let warningText = document.querySelector('.warning-text'); /*1*/
-let errorMessage = document.querySelector('.container-error'); /*2*/
-let dataOutput = document.querySelector('.data-output'); /*3*/
-let pDefault = document.querySelector('.pDefault'); /*none*/
+let warningText = document.querySelector('.warning-text');
+let errorMessage = document.querySelector('.container-error');
+let dataOutput = document.querySelector('.data-output');
+let pDefault = document.querySelector('.pDefault');
 let infoCopied = document.querySelector('.copied');
 
 
 /* Show functions */
+
+/**
+ * `showDataOutput()` is a function that removes the class `none` from the element with the id
+ * `dataOutput` and adds the class `none` to the element with the id `pDefault`
+ */
 function showDataOutput(){
   dataOutput.classList.remove('none');
   pDefault.classList.add('none');
 }
 
+/**
+ * If the default paragraph is visible, hide it and show the error message.
+ */
 function showErrorMessage(){
   pDefault.classList.add('none');
   errorMessage.classList.remove('none');
 }
 
+/**
+ * It removes the class 'none' from the paragraph with the id 'pDefault' and the paragraph with the id
+ * 'warningText'. Then it adds the class 'none' to the paragraph with the id 'warningText' after 3
+ * seconds
+ */
 function shoWarningText(){
   pDefault.classList.remove('none');
   warningText.classList.remove('none');
@@ -41,17 +54,32 @@ function shoWarningText(){
   }, 3000);
 }
 
+
 /* Principal functions */
+
+/**
+ * It adds the class 'none' to the errorMessage and dataOutput elements
+ */
 function clearDataOutput(){
   errorMessage.classList.add('none');
   dataOutput.classList.add('none');
 }
 
+/**
+ * It takes a string as an argument and returns true if the string contains only letters and spaces,
+ * and false if it contains anything else
+ * @param string - The string to be validated.
+ * @returns A boolean value.
+ */
 function validateInput(string) {
   const regex = /^([a-zA-Z]+[\s]*)+$/;
   return regex.test(string);
 }
 
+/**
+ * It replaces all vowels with numbers, then replaces all numbers with the corresponding vowel's name
+ * @param string - The string to be encrypted.
+ */
 function encrypt(string) {
   let encryptedText = "";
   encryptedText = string
@@ -70,6 +98,11 @@ function encrypt(string) {
   showDataOutput();
 }
 
+/**
+ * It takes a string, replaces all instances of the encrypted vowels with their decrypted counterparts,
+ * and then outputs the decrypted text to the output area.
+ * @param string - The string to be decrypted.
+ */
 function decrypt(string) {
   let decryptedText = "";
   decryptedText = string
@@ -82,20 +115,33 @@ function decrypt(string) {
   showDataOutput();
 }
 
+/**
+ * It removes the light background and adds the dark background.
+ */
 function darkMode(){
+  body.classList.remove('gradient-background--light');
   body.classList.toggle('darkMode');
+  body.classList.add('gradient-background--dark');
   buttonDark.classList.add('none');
   buttonLight.classList.remove('none');
 }
 
+/**
+ * It removes the darkMode class and the gradient-background--dark class from the body element. It then
+ * adds the gradient-background--light class to the body element. It also adds the none class to the
+ * buttonLight element and removes the none class from the buttonDark element.
+ */
 function lightMode(){
-  body.classList.remove('darkMode');
+  body.classList.remove('darkMode', 'gradient-background--dark');
+  body.classList.add('gradient-background--light');
   buttonLight.classList.add('none');
   buttonDark.classList.remove('none');
 }
 
 
 /* Buttons Events */ 
+
+
 buttonEncrypt.addEventListener('click', ()=>{
   clearDataOutput();
   const inputText = inputArea.value;
